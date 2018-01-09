b12efc74afc96cc4d27955469a3f0c26f3e4b45c
example name <974632489@qq.com>
2018-01-09 19:46:48 +0800

    第一次提交

---
+++ src/initialService/doInitServlet.java
@@ -0,0 +1,40 @@
+package initialService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import medialive.DAO.impl.liveDAOImpl;
+
+public class doInitServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("初始化initServlet");
+		liveDAOImpl liveDAO = new liveDAOImpl();
+		liveDAO.clear();
+	}
+
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("这是初始化servlet,不做业务用");
+	}
+	
+	
+
+	
+	
+
+}
