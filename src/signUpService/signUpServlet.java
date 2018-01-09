348d3d0d12d9753e55a003bdbb0284b9b72e8126
example name <974632489@qq.com>
2018-01-09 20:00:04 +0800

    初始化servlet完成  signUp servlet 可以成功访问

---
+++ src/signUpService/signUpServlet.java
@@ -0,0 +1,24 @@
+package signUpService;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class signUpServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("’‚¿Ô «signUpServlet");
+	}
+	
+
+}
