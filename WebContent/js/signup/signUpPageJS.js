8dfddc1d2d6f62be5c9c57d29d036df04781cbaa
example name <974632489@qq.com>
2018-01-10 20:45:09 +0800

    解决多次请求响应消息堆叠显示的问题

--- WebContent/js/signup/signUpPageJS.js
+++ WebContent/js/signup/signUpPageJS.js
@@ -5,10 +5,18 @@
 var func = function() {
 	var url = "/medialive/signUp"
 	var param = $(".signUpinput");
-	console.log("roomName:"+param[0].value+","+"streamName:"+param[1].value)
-	$.post(url,{ roomName: param[0].value, streamName: param[1].value },
-			function(data){
-		console.log(data);
+	console.log("roomName:" + param[0].value + "," + "streamName:"
+			+ param[1].value)
+	$.post(url, {
+		roomName : param[0].value,
+		streamName : param[1].value
+	}, function(data) {
+		var a = $("<a></a>");
+		a.text(data);
+		a.css("color","red");
+		a.attr("class","response");
+		$("#signUpForm").children().filter(".response").remove();
+		$("#signUpForm").append(a);
 	});
 
 }
