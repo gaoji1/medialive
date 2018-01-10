85bbbbd000d74ef2c0b88ba8b910222e7e3503ed
example name <974632489@qq.com>
2018-01-10 15:39:32 +0800

    livePage页面交互基本雏形完成

--- WebContent/js/live/livePageJS.js
+++ WebContent/js/live/livePageJS.js
@@ -4,7 +4,19 @@
 var fun = function() {
 	var url = "/medialive/live";
 	$.get(url,function(data){
-		console.log(data);
+		
+		var json = JSON.parse(data);
+		for(x in json){
+			var row = $("<tr></tr>");
+			var id = $("<td>"+json[x].id+"</td>");
+			var roomtd = $("<td></td>")
+			var room = $("<a>"+json[x].roomName+"</a>");
+			room.attr("href","/medialive/livePlay.html?id="+json[x].id);
+			roomtd.append(room);
+			var stream = $("<td>"+json[x].streamName+"</td>");
+			row.append(id).append(roomtd).append(stream);
+			$("#liveTable").append(row);
+		}
 	});
 }
 $(fun);
--- src/liveService/liveServlet.java
+++ src/liveService/liveServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSON;
+
 import medialive.DAO.impl.liveDAOImpl;
 import medialive.domain.live;
 
@@ -23,10 +25,16 @@ public class liveServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("doGET");
-		resp.setCharacterEncoding("utf-8");
+//		从数据库中拉取直播间信息
 		liveDAOImpl liveDAO = new liveDAOImpl();
 		List<live> liveList = liveDAO.list();
-		System.out.println(liveList);
+//		直播间信息转换成json字符串
+		String jsonString = JSON.toJSONString(liveList);
+		System.out.println(jsonString);
+//		设置输出流编码格式并输出
+		resp.setCharacterEncoding("utf-8");
+		resp.getWriter().println(jsonString);
+		
 	}
 	
 	
