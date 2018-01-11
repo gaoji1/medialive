c5b58c4aeb64d75b9278444bfd6528ff2fb46efa
example name <974632489@qq.com>
2018-01-11 09:25:18 +0800

    整理代码格式,加一些注释,删除调试时的浏览器控制台输出

--- WebContent/js/signup/signUpPageJS.js
+++ WebContent/js/signup/signUpPageJS.js
@@ -3,18 +3,26 @@
  */
 
 var func = function() {
+	// 设置请求url
 	var url = "/medialive/signUp"
+	// 获取inpu标签
 	var param = $(".signUpinput");
-	console.log("roomName:" + param[0].value + "," + "streamName:"
-			+ param[1].value)
+	// 发送post AJAX请求
+	/**
+	 * 请求成功执行 
+	 * 1)生成<a>标签 
+	 * 2)把传回的数据(直播间验证信息)加到<a>标签中 
+	 * 3)设置<a>标签颜色为红色 
+	 * 4)把<a>标签加入到表单最后
+	 */
 	$.post(url, {
 		roomName : param[0].value,
 		streamName : param[1].value
 	}, function(data) {
 		var a = $("<a></a>");
 		a.text(data);
-		a.css("color","red");
-		a.attr("class","response");
+		a.css("color", "red");
+		a.attr("class", "response");
 		$("#signUpForm").children().filter(".response").remove();
 		$("#signUpForm").append(a);
 	});
--- src/signUpService/signUpServlet.java
+++ src/signUpService/signUpServlet.java
@@ -21,34 +21,33 @@ public class signUpServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("signUp doPOST");
-//		设置响应编码
+		// 设置响应编码
 		resp.setCharacterEncoding("utf-8");
-//		获取DAO对象
+		// 获取DAO对象
 		liveDAOImpl liveDAO = new liveDAOImpl();
-//		获取POST请求参数
+		// 获取POST请求参数
 		String roomName = req.getParameter("roomName");
 		String streamName = req.getParameter("streamName");
-//		查询数据库
+		// 查询数据库
 		live liveDemo_1 = liveDAO.getByroom(roomName);
 		live liveDemo_2 = liveDAO.getBystream(streamName);
-//		判断直播间是否存在
-		if(liveDemo_1.getId()==null&&liveDemo_2.getId()==null) {
+		// 判断直播间是否存在
+		if (liveDemo_1.getId() == null && liveDemo_2.getId() == null) {
 			live liveDemo = new live();
 			liveDemo.setRoomName(roomName);
 			liveDemo.setStreamName(streamName);
 			liveDemo.setRed5URL("/myApp");
 			liveDAO.save(liveDemo);
 			resp.getWriter().println("直播间注册成功!");
-			
-		}else if(liveDemo_1.getId()!=null&&liveDemo_2.getId()==null) {
+
+		} else if (liveDemo_1.getId() != null && liveDemo_2.getId() == null) {
 			resp.getWriter().println("您的直播间命名冲突,请重新为直播间起个名字");
-		}else if(liveDemo_1.getId()==null&&liveDemo_2.getId()!=null) {
+		} else if (liveDemo_1.getId() == null && liveDemo_2.getId() != null) {
 			resp.getWriter().println("您的直播流名称已被占用,请重新命名");
-		}else {
+		} else {
 			resp.getWriter().println("直播间名称与流名称均冲突,请重新命名");
 		}
-				
+
 	}
-	
 
 }
