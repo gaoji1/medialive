b2288423ade15644b050bfbae623375d63fc642c
example name <974632489@qq.com>
2018-01-11 12:19:19 +0800

    在liveplay页面成功接收请求行参数(id)

--- WebContent/js/live/livePlayJS.js
+++ WebContent/js/live/livePlayJS.js
@@ -16,20 +16,39 @@
  * @param {Object}
  *            divID 显示的divID
  */
-alert(1);
-var func = function(){
-	var so = new SWFObject("../player.swf","CuSunPlayer","700", "410", "9", "#000000");
-	so.addParam("allowfullscreen","true");
-	so.addParam("allowscriptaccess","always");
-	so.addParam("wmode","opaque");
-	so.addParam("quality","high");
-	so.addParam("salign","lt");
-	//播放器设置文件-----------------------------
-	so.addVariable("JcScpFile","CuSunV4setLive.xml"); //配置文件
-	so.addVariable("JcScpServer","rtmp://localhost/myApp"); //流媒体服务器
-	so.addVariable("JcScpVideoPath","firststream"); //直播流名称
-	so.addVariable("JcScpAutoPlay","yes"); //是否自动播放
-	so.addVariable("JcScpSharetitle","欢迎来到直播间"); //视频标题
+
+function getAddressURLParam(paramName)  
+{  
+     //构造一个含有目标参数的正则表达式的对象  
+     var reg = new RegExp("(^|&)" + paramName + "=([^&]*)(&|$)");  
+     //匹配目标参数  
+     var url = window.location.search.substr(1).match(reg);  
+    //返回参数值  
+    if(url != null)  
+     return unescape(url[2]);  
+    return null;  
+}  
+
+var playerFunc = function() {
+	var so = new SWFObject("player.swf", "CuSunPlayer", "700", "410", "9",
+			"#000000");
+	so.addParam("allowfullscreen", "true");
+	so.addParam("allowscriptaccess", "always");
+	so.addParam("wmode", "opaque");
+	so.addParam("quality", "high");
+	so.addParam("salign", "lt");
+	// 播放器设置文件-----------------------------
+	so.addVariable("JcScpFile", "CuSunV4setLive.xml"); // 配置文件
+	so.addVariable("JcScpServer", "rtmp://localhost/myApp"); // 流媒体服务器
+	so.addVariable("JcScpVideoPath", "firststream"); // 直播流名称
+	so.addVariable("JcScpAutoPlay", "yes"); // 是否自动播放
+	so.addVariable("JcScpSharetitle", "欢迎来到直播间"); // 视频标题
 	so.write("CuPlayer");
 }
 
+var func = function() {
+	var id = getAddressURLParam("id");
+	alert(id);
+}
+
+$(func)
